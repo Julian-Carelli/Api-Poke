2f973ab1771b1c1d05d7e389da7865a5758245a0
Julian-Carelli <julianignaciocarelli@gmail.com>
2021-04-12 14:39:21 -0300

    fix issues

---
+++ src/components/Presentation/style.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components'
+
+export const PresentationStyled = styled.section.attrs({
+    className:'Presentation container'
+})`
+
+
+.Presentation__row {
+    padding:45px 0px;
+}
+
+`;
